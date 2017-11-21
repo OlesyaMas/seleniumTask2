b4a0a44ac4190e596108c6837f67c667ee0b5c6b
Olesya Maslyuk <Olesya_Maslyuk@epam.com>
2017-11-21 15:16:44 +0200

    admin and login task

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,63 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+public class Launcher {
+    public static void main(String[] args) {
+
+        WebDriver driver = initChromeDriver();
+        driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
+
+        WebElement loginField = driver.findElement(By.id("email"));
+        loginField.sendKeys("webinar.test@gmail.com");
+
+        try{
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException ie){
+        }
+
+        WebElement passField = driver.findElement(By.id("passwd"));
+        passField.sendKeys("Xcg7299bnSmMuRLp9ITw");
+
+        WebElement authButton = driver.findElement(By.name("submitLogin"));
+        authButton.click();
+
+        try{
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException ie){
+        }
+
+        WebElement avatarButton = driver.findElement(By.id("employee_infos"));
+        avatarButton.click();
+
+        try{
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException ie){
+        }
+
+        WebElement logoutButton = driver.findElement(By.id("header_logout"));
+        logoutButton.click();
+
+
+        /*List<WebElement> list = driver.findElements(By.className("b_algo"));
+        System.out.println(list.size());*/
+
+        /*List<WebElement> refers = driver.findElement(By.tagName("ol")).findElements(By.className("b_algo"));
+        System.out.println(refers.size());*/
+    }
+
+    public static WebDriver initChromeDriver (){
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/drivers/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        return new ChromeDriver(options);
+    }
+}
+
